bed4651929294456eed36e9ceeb1eb331e11d965
Pasi Lallinaho <pasi@shimmerproject.org>
2021-04-30 11:15:59 +0300

    Add animation for most commonly used text elements (#10)

--- script.js
+++ script.js
@@ -6,4 +6,35 @@ jQuery( function( e ) {
 	jQuery( '.image-information .icon' ).on( 'click', function( e ) {
 		jQuery( this ).closest( '.image-information' ).toggleClass( 'information-visible' );
 	} );
+
+	jQuery.fn.isAlmostInViewport = function( top_offset ) {
+		if( !top_offset ) {
+			top_offset = 100;
+		}
+
+		var elementTop = jQuery( this ).children( ).first( ).offset( ).top + top_offset;
+		var elementBottom = elementTop + jQuery( this ).outerHeight( );
+
+		var viewportTop = jQuery( window ).scrollTop( );
+		var viewportBottom = viewportTop + jQuery( window ).height( );
+
+		console.log( jQuery( this ).children( ).first( ).text( ) );
+
+		return elementBottom > viewportTop && elementTop < viewportBottom;
+	};
+
+	jQuery( '.wp-block-media-text__content, .wp-block-cover__inner-container' ).each( function( e ) {
+		jQuery( this ).addClass( 'fade-in-up is-hidden' );
+	} );
+
+	jQuery( window ).resize( checkAnimations ).scroll( checkAnimations );
+	checkAnimations( );
 } );
+
+function checkAnimations( ) {
+	jQuery( '.fade-in-up:not(.is-visible)' ).each( function( e ) {
+		if( jQuery( this ).isAlmostInViewport( ) ) {
+			jQuery( this ).addClass( 'is-visible' ).removeClass( 'is-hidden' );
+		}
+	} );
+}
