bea1c0b26ad4dbb4c45252116783b7b54263d0ae
Pasi Lallinaho <pasi@shimmerproject.org>
2022-02-16 19:25:52 +0200

    Alustava sisältö "paremmalle" 404-sivulle.

--- index.php
+++ index.php
@@ -5,8 +5,16 @@
 			if( have_posts( ) ) {
 				get_template_part( 'content', get_post_type( ) );
 			} else {
-				echo '<h1>' . __( 'Content not found.', 'ropecon' ) . '</h1>';
-				echo '<p>' . __( 'The content you were looking for was not found.', 'ropecon' ) . '</p>';
+				?>
+				<section id="content" <?php post_class( 'site group' ) ?>>
+					<div class="wp-block-cover alignfull is-style-full-height"><img class="wp-block-cover__image-background" src="<?php echo get_stylesheet_directory_uri( ); ?>/images/block-pattern-preview/title-cover.svg" data-object-fit="cover">
+						<div class="wp-block-cover__inner container" style="z-index: 1000;">
+							<h1 class="has-text-align-center has-white-color has-text-color"><?php _e( 'Content not found', 'ropecon' ); ?></h1>
+							<p class="has-text-align-center has-white-color has-text-color"><?php _e( 'The content you were looking for was not found.', 'ropecon' ); ?></p>
+						</div>
+					</div>
+				</section>
+				<?php
 			}
 		?>
 	</main>
