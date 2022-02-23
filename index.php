c852f0c8948352c654404085df3beefdea0c8502
Pasi Lallinaho <pasi@shimmerproject.org>
2022-02-23 13:23:52 +0200

    Ajetaan the_content -filtteri 404 -sivulle, jotta saadaan kuvan tekijänoikeustiedot näkyviin

--- index.php
+++ index.php
@@ -9,11 +9,12 @@
 				// 404
 				if( get_option( 'ropecon_404_page' ) > 0 ) {
 					if( function_exists( 'pll_get_post' ) ) {
-						$post = pll_get_post( get_option( 'ropecon_404_page' ) );
-						echo get_the_content( null, false, $post );
+						$post_id = pll_get_post( get_option( 'ropecon_404_page' ) );
 					} else {
-						echo get_the_content( null, false, get_option( 'ropecon_404_page' ) );
+						$post_id = get_option( 'ropecon_404_page' );
 					}
+
+					echo apply_filters( 'the_content', get_the_content( null, false, $post_id ) );
 				} else {
 					?>
 						<div class="wp-block-cover alignfull is-style-full-height"><img class="wp-block-cover__image-background" src="<?php echo get_stylesheet_directory_uri( ); ?>/images/block-pattern-preview/title-cover.svg" data-object-fit="cover">
