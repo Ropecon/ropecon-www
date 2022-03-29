f3bbb0a9df5e5e9fcf9e22988c804c9bb4674985
Pasi Lallinaho <pasi@shimmerproject.org>
2022-03-29 14:08:31 +0300

    Siirretään metadatan kerääminen functions.php-tiedostoon

--- functions.php
+++ functions.php
@@ -172,6 +172,102 @@ function ropecon_styles_scripts( ) {
 	wp_enqueue_style( get_template( ) . '-menu', get_template_directory_uri( ) . '/style-menu.css', array( get_template( ) ), $ver );
 }
 
+/**
+ *  Add functions for custom title and description output
+ *
+ */
+
+function ropecon_wp_title( $echo = false ) {
+	$title = wp_title( '&laquo;', false, 'right' ) . get_bloginfo( 'name' );
+	if( !$echo ) {
+		return $title;
+	} else {
+		echo $title;
+	}
+}
+
+function ropecon_description( $echo = false ) {
+	if( has_excerpt( ) ) {
+		$description = get_the_excerpt( );
+	} elseif( strlen( get_bloginfo( 'description' ) ) > 0 ) {
+		$description = get_bloginfo( 'description' );
+	} else {
+		$description = get_bloginfo( 'name' );
+	}
+
+	if( !$echo ) {
+		return $description;
+	} else {
+		echo $description;
+	}
+}
+
+/**
+ *  Add metadata to <head>
+ *
+ */
+
+function ropecon_wp_head_meta( ) {
+	global $wp;
+
+	$title = ropecon_wp_title( );
+	$description = ropecon_description( );
+
+	$meta_url = home_url( add_query_arg( array(), $wp->request ) );
+	$meta_domain = parse_url( home_url( ) )['host'];
+
+	$post_id = 0;
+	$meta_image = get_stylesheet_directory_uri( ) . '/images/meta-default.svg';
+
+	if( ( is_front_page( ) && is_home( ) ) || is_home( ) ) {
+		$post_id = get_option( 'page_for_posts' );
+	} elseif( !is_404( ) ) {
+		global $post;
+		$post_id = $post->ID;
+		$post_content = $post->post_content;
+	}
+
+	if( $post_id > 0 ) {
+		if( !isset( $post_content ) ) {
+			$post_content = get_the_content( null, false, $post_id );
+		}
+
+		if( get_the_post_thumbnail( $post_id ) != '' ) {
+			$meta_image = get_the_post_thumbnail_url( $post_id, array( 1200, 630 ) );
+	 	} else {
+		 	$content = preg_match_all('/<img.+?src=[\'"]([^\'"]+)[\'"].*?>/i', $post_content, $matches );
+		 	if( !empty( $matches[1][0] ) ) {
+				// first image is the image
+				$meta_image = $matches[1][0];
+			}
+		}
+	}
+
+	echo PHP_EOL;
+	// FB Meta
+	echo '<meta property="og:url" content="' . $meta_url . '">' . PHP_EOL;
+	echo '<meta property="og:type" content="website">' . PHP_EOL;
+	echo '<meta property="og:title" content="' . $title . '">' . PHP_EOL;
+	echo '<meta property="og:description" content="' . $description . '">' . PHP_EOL;
+	if( !empty( $meta_image ) ) {
+		echo '<meta property="og:image" content="' . $meta_image . '">' . PHP_EOL;
+	}
+	echo PHP_EOL;
+
+	// Twitter meta
+	echo '<meta name="twitter:card" content="summary_large_image">' . PHP_EOL;
+	echo '<meta property="twitter:domain" content="' . $meta_domain . '">' . PHP_EOL;
+	echo '<meta property="twitter:url" content="' . $meta_url . '">' . PHP_EOL;
+	echo '<meta name="twitter:title" content="' . $title . '">' . PHP_EOL;
+	echo '<meta name="twitter:description" content="' . $description . '">' . PHP_EOL;
+	if( !empty( $meta_image ) ) {
+		echo '<meta name="twitter:image" content="' . $meta_image . '">' . PHP_EOL;
+	}
+	echo PHP_EOL;
+}
+
+add_action( 'wp_head', 'ropecon_wp_head_meta', 1 );
+
 /*
  *  Gutenberg
  *
--- header.php
+++ header.php
@@ -1,59 +1,14 @@
 <!DOCTYPE html>
-<?php
-	$title = wp_title( '&laquo;', false, 'right' ) . get_bloginfo( 'name' );
-
-	if( has_excerpt( ) ) {
-		$description = get_the_excerpt( );
-	} elseif( strlen( get_bloginfo( 'description' ) ) > 0 ) {
-		$description = get_bloginfo( 'description' );
-	} else {
-		$description = get_bloginfo( 'name' );
-	}
-
-	$meta_url = home_url( add_query_arg( array(), $wp->request ) );
-	$meta_domain = parse_url( home_url( ) )['host'];
-
-	$meta_image = '';
-
-	global $post;
-	if( get_the_post_thumbnail( $post->ID ) != '' ) {
-		$meta_image = get_the_post_thumbnail_url( null, array( 1200, 630 ) );
- 	} else {
-	 	$content = preg_match_all('/<img.+?src=[\'"]([^\'"]+)[\'"].*?>/i', $post->post_content, $matches );
-	 	if( !empty( $matches[1][0] ) ) {
-			// first image is the image
-			$meta_image = $matches[1][0];
-		} else {
-			// no images in post found, fallback to default
-			$meta_image = get_stylesheet_directory_uri( ) . '/images/meta-default.png';
-		}
-	}
-?>
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes( 'xhtml' ); ?>>
 <head>
-	<title><?php echo $title; ?></title>
+	<title><?php ropecon_wp_title( true ); ?></title>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 
 	<meta name="copyright" content="&copy; Ropecon" />
-	<meta name="description" content="<?php echo $description; ?>" />
+	<meta name="description" content="<?php ropecon_description( true ); ?>" />
 
 	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri( ); ?>/images/ropecon_icon.png">
 	<meta name="viewport" content="width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1" />
-
-	<!-- FB meta -->
-	<meta property="og:url" content="<?php echo $meta_url; ?>">
-	<meta property="og:type" content="website">
-	<meta property="og:title" content="<?php echo $title; ?>">
-	<meta property="og:description" content="<?php echo $description; ?>">
-	<meta property="og:image" content="<?php echo $meta_image; ?>">
-
-	<!-- Twitter meta -->
-	<meta name="twitter:card" content="summary_large_image">
-	<meta property="twitter:domain" content="<?php echo $meta_domain; ?>">
-	<meta property="twitter:url" content="<?php echo $meta_url; ?>">
-	<meta name="twitter:title" content="<?php echo $title; ?>">
-	<meta name="twitter:description" content="<?php echo $description; ?>">
-	<meta name="twitter:image" content="<?php echo $meta_image; ?>">
 
 	<?php wp_head( ); ?>
 </head>
